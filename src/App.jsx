7cf0998dc4814042aa7c87152f70746b31fc99a5
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-05-16 13:48:31 -0600

    todos mapped and tested

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import useTodoReducer from "./hooks/useTodoReducer";
 import { Header } from "./components/Header";
 import TodoForm from "./components/TodoForm";
 import "./App.css";
 
 function App() {
+  const [todos, dispatch] = useTodoReducer();
+
+  function handleAddTodo(text) {
+    dispatch({ type: "addTodo", text });
+  }
   return (
     <>
       <div className="container">
         <Header />
-        <TodoForm />
+        <TodoForm onAddTodo={handleAddTodo} />
+        <ul>
+          {todos.map((todo) => {
+            <li key={todo.id}>{todo.text}</li>;
+            console.log(todo.text);
+          })}
+        </ul>
       </div>
     </>
   );
