a6f37968a885855625b2165e6bc5246aa7b499de
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-05-16 13:36:48 -0600

    todo adding form createtd and tested

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Header } from "./components/Header";
+import TodoForm from "./components/TodoForm";
 import "./App.css";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <div className="container">
         <Header />
+        <TodoForm />
       </div>
     </>
   );
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+const TodoForm = ({ onAddTodo }) => {
+  const [todo, setTodo] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (todo.trim()) {
+      onAddTodo(todo);
+      setTodo("");
+    }
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => {
+            setTodo(e.target.value);
+          }}
+          placeholder="Add todo..."
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+    </>
+  );
+};
+
+export default TodoForm;
